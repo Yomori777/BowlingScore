570b46cbe2e55092ed9e7a47bc4f333979ed16bd
Yomori777 <z42u1mfc1rzd60iuq2x6@docomo.ne.jp>
2024-01-07 09:44:28 +0900

    Adding necessary functions

--- Bowling/BowlingGame.cs
+++ Bowling/BowlingGame.cs
@@ -1,11 +1,20 @@
 ﻿
 
+
+
+using System;
+using System.Reflection;
+
 namespace Bowling
 {
     public class BowlingGame
     {
         Frames _frames = new();
         public int Score => _frames.GetTotalScore();
+
+        public int FrameNumber => _frames.FrameNumber;
+
+        public bool IsEnd => _frames.IsEnd;
 
         public void Bowl(int pin)
         {
@@ -17,5 +26,14 @@ namespace Bowling
             return _frames.GetFrameScore(index);
         }
 
+        public bool IsSpare(int frameNumber)
+        {
+            return _frames.IsSpare(frameNumber);
+        }
+
+        public bool IsStrike(int frameNumber)
+        {
+            return _frames.IsStrike(frameNumber);
+        }
     }
 }
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace TestProject1
             game.Bowl(2);
             game.Bowl(5);
             Assert.IsTrue(game.Score == 7);
-            Assert.IsTrue(game.GetFrameScore(1) == 7);
+            Assert.IsTrue(game.GetFrameScore(0) == 7);
         }
         [TestMethod]
         public void TestSpare()
@@ -29,9 +29,9 @@ namespace TestProject1
             game.Bowl(5);
             Assert.IsTrue(game.Score == 10);
             game.Bowl(5);
-            Assert.IsTrue(game.GetFrameScore(1) == 15);
+            Assert.IsTrue(game.GetFrameScore(0) == 15);
             game.Bowl(4);
-            Assert.IsTrue(game.GetFrameScore(2) == 9);
+            Assert.IsTrue(game.GetFrameScore(1) == 9);
             Assert.IsTrue(game.Score == 24);
         }
         [TestMethod]
@@ -42,8 +42,8 @@ namespace TestProject1
             Assert.IsTrue(game.Score == 10);
             game.Bowl(5);
             game.Bowl(4);
-            Assert.IsTrue(game.GetFrameScore(1) == 19);
-            Assert.IsTrue(game.GetFrameScore(2) == 9);
+            Assert.IsTrue(game.GetFrameScore(0) == 19);
+            Assert.IsTrue(game.GetFrameScore(1) == 9);
             Assert.IsTrue(game.Score == 28);
         }
         [TestMethod]
@@ -55,6 +55,47 @@ namespace TestProject1
                 game.Bowl(10);
             }
             Assert.IsTrue(game.Score == 300);
+        }
+        [TestMethod]
+        public void TestGetFrameNumber()
+        {
+            var game = new BowlingGame();
+            Assert.IsTrue(game.FrameNumber == 0);
+            game.Bowl(5);
+            Assert.IsTrue(game.FrameNumber == 0);
+            game.Bowl(4);
+            Assert.IsTrue(game.FrameNumber == 1);
+            game.Bowl(10);
+            Assert.IsTrue(game.FrameNumber == 2);
+        }
+        [TestMethod]
+        public void TestFrameState()
+        {
+            var game = new BowlingGame();
+            game.Bowl(5);
+            game.Bowl(4);
+            Assert.IsTrue(false == game.IsStrike(0));
+            Assert.IsTrue(false == game.IsSpare(0));
+            game.Bowl(10);
+            Assert.IsTrue(true == game.IsStrike(1));
+            Assert.IsTrue(false == game.IsSpare(1));
+            game.Bowl(5);
+            game.Bowl(5);
+            Assert.IsTrue(false == game.IsStrike(2));
+            Assert.IsTrue(true == game.IsSpare(2));
+        }
+        [TestMethod]
+        public void TestFrameEnd()
+        {
+            var game = new BowlingGame();
+            game.Bowl(5);
+            game.Bowl(4);
+            foreach (var _ in Enumerable.Range(0,11))
+            {
+                Assert.IsTrue(false == game.IsEnd);
+                game.Bowl(10);
+            }
+            Assert.IsTrue(true == game.IsEnd);
         }
     }
 }
